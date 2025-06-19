7ad4065c56eb514060d5362394ac01308db5b212
Sofia <sofiagorgees@gmail.com>
2025-06-19 16:25:00 -0500

    am working on implementing file search tool

--- main2.py
+++ main2.py
@@ -1,20 +1,51 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
 from azure.ai.agents.models import ListSortOrder
 
+# from azure.ai.inference import ChatCompletionsClient
+# from azure.ai.inference.models import SystemMessage, UserMessage
+# from azure.core.credentials import AzureKeyCredential
+
+# Create project client with default azure credentials and endpoint from environment variable
 project = AIProjectClient(
     credential=DefaultAzureCredential(),
-    endpoint="https://sgorgees-exploring-resource.services.ai.azure.com/api/projects/sgorgees-exploring")
+    endpoint=os.getenv("AZURE_FOUNDRY_ENDPOINT"),
+    )    
 
+# agentIdList = ["asst_1DYZAop1Obe8KQSVucC7JcpW", "asst_HIq5x9Gelzk6PlfX30LpIMxy", "asst_yksk9SIQhvbf4dCLnoKHPNM8", "asst_w79HcJ15MGIat8yvZNcW4kqs", "asst_DXpD2zd3Uc3t6gRxu9IdBl8Y"]
 
-agent = project.agents.get_agent("asst_fGV7F0M0Vic9UEzL2YlUGFVW")
+#Chose and agent and model to chat with
+print("Which agent would you like to chat with?")
+agents = ["Image Conscious Enthusiast","Practical DIYer","Car-Involved Tinker", "Diligent Delegator", "Disinterested Value Seeker"]
+for i in range(len(agents)):
+    print(f"{i + 1}. {agents[i]}")
 
+agent_choice = input("Enter the number of the agent you want to chat with: ")
+print("Which model would you like to use?\n\t1. gpt-4o\n\t2. gpt-4.1")
+model_choice = input("Enter the number of the model you want to use: ")
+if model_choice == "1":
+    model = "gpt-4o"
+elif model_choice == "2":
+    model = "gpt-4.1"
+
+# Create a new agent with the selected model and name
+agent = project.agents.create_agent(
+    model=model,
+    name = agents[int(agent_choice) - 1],
+    instructions = "REPLACE WITH INSTRUCTIONS"
+)
+#Create new thread
 thread = project.agents.threads.create()
 print(f"Created thread, ID: {thread.id}")
 
-chat = True
-print('Hello, I am the image conscious enthusiast model!\n If you want to end the chat type exit ,quit, q')
-while chat:
+print('Hello, I am the ' + agents[int(agent_choice) - 1] + ' model!\n If you want to end the chat type exit ,quit, q')
+
+# Loop to chat with the agent
+while True:
     user_prompt = input("\n-You: ")
     if user_prompt.lower() in ["exit", "quit", "q"]:
         print("Ending chat session.")
@@ -39,3 +70,9 @@ while chat:
         for message in messages:
             if message.text_messages:
                 print(f"{message.role}: {message.text_messages[-1].text.value}")
+
+project.agents.delete_agent(agent.id)
+print(f"Deleted agent, ID: {agent.id}")
+
+    
+
